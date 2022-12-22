8d40bbf49b11c75d6c6af0b5cc8ef7c673d3bea8
Luã Bida Vacaro <82233055+luabida@users.noreply.github.com>
2022-12-22 13:49:10 -0300

    Update reanalysis_era5_single_levels.sql

--- satellite_weather_downloader/reanalysis_era5_single_levels.sql
+++ satellite_weather_downloader/reanalysis_era5_single_levels.sql
@@ -1,16 +1,32 @@
-CREATE TABLE "Municipio".weather_copernicus (
-    date TIMESTAMP WITHOUT TIME ZONE, 
-    geocodigo BIGINT, 
-    temp_min FLOAT(23), 
-    temp_med FLOAT(23), 
-    temp_max FLOAT(23), 
-    precip_min FLOAT(23), 
-    precip_med FLOAT(23), 
-    precip_max FLOAT(23), 
-    pressao_min FLOAT(23), 
-    pressao_med FLOAT(23), 
-    pressao_max FLOAT(23), 
-    umid_min FLOAT(23), 
-    umid_med FLOAT(23), 
+CREATE TABLE "Municipio".copernicus_br (
+    date TIMESTAMP WITHOUT TIME ZONE,
+    geocodigo BIGINT,
+    temp_min FLOAT(23),
+    temp_med FLOAT(23),
+    temp_max FLOAT(23),
+    precip_min FLOAT(23),
+    precip_med FLOAT(23),
+    precip_max FLOAT(23),
+    pressao_min FLOAT(23),
+    pressao_med FLOAT(23),
+    pressao_max FLOAT(23),
+    umid_min FLOAT(23),
+    umid_med FLOAT(23),
     umid_max FLOAT(23)
 );
+
+CREATE INDEX CONCURRENTLY IF NOT EXISTS date_idx ON "Municipio".copernicus_br (date);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS geocodigo_idx ON "Municipio".copernicus_br (geocodigo);
+
+-- Foz do Iguaçu
+CREATE TABLE "Municipio".copernicus_foz (
+    date TIMESTAMP WITHOUT TIME ZONE,
+    geocodigo BIGINT,
+    temp FLOAT(23),
+    precip FLOAT(23),
+    pressao FLOAT(23),
+    umid FLOAT(23)
+);
+
+CREATE INDEX CONCURRENTLY IF NOT EXISTS date_idx ON "Municipio".copernicus_foz (date);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS geocodigo_idx ON "Municipio".copernicus_foz (geocodigo);
