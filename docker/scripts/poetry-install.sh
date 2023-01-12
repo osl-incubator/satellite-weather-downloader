1205012f9e3401eaebc3e4d03288ed0d02f37cb3
Luã Bida Vacaro <luabidaa@gmail.com>
2023-01-12 09:41:55 -0300

    Poetry install on docker

--- docker/scripts/poetry-install.sh
+++ docker/scripts/poetry-install.sh
@@ -6,6 +6,5 @@ if [[ $ENV == "prod" ]]; then
   export POETRY_INSTALL_ARGS="--no-dev"
 fi
 
-curl -sSL https://install.python-poetry.org | python3 -
 poetry config virtualenvs.create false
 poetry install $POETRY_INSTALL_ARGS
