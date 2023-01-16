848d64dc9cd394a43aa26a5ed245d3385797f5e8
Luã Bida Vacaro <luabidaa@gmail.com>
2023-01-16 09:46:48 -0300

    Using 15 concurrently tasks to download netcdf files

--- docker/celeryapp/downloader/config.py
+++ docker/celeryapp/downloader/config.py
@@ -10,7 +10,7 @@ broker_url = os.getenv('CELERY_BROKER_URL')
 # List of modules to import when the Celery worker starts.
 accept_content = ['application/json']
 result_serializer = 'json'
-worker_concurrency = 6
+worker_concurrency = 15
 worker_max_tasks_per_child = 10
 timezone = 'America/Sao_Paulo'
 task_always_eager = False
