dcb2521230f733b8388c0cb149750f7d0dd31375
Luã Bida Vacaro <82233055+luabida@users.noreply.github.com>
2023-01-23 16:57:41 -0300

    fix(ci): remove unecessary files to release

--- satellite_downloader/utils/__init__.py
+++ satellite_downloader/utils/__init__.py
@@ -1,15 +1,2 @@
 # type: ignore[attr-defined]
-"""satellite_weather_downloader Weather Downloader Python package"""
-from importlib import metadata as importlib_metadata
-from pathlib import Path
-
-
-def get_version() -> str:
-    try:
-        return importlib_metadata.version(__name__)
-    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
-        return '1.4.0'  # changed by semantic-release
-
-
-version: str = get_version()
-__version__: str = version
+# TODO: Add docstrings
