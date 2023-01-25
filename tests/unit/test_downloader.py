320e06dad4101d3642e2a88a8cd1b67e58face27
Luã Bida Vacaro <luabidaa@gmail.com>
2023-01-25 16:48:49 -0300

    Add tests for date format

--- tests/unit/test_downloader.py
+++ tests/unit/test_downloader.py
@@ -4,6 +4,7 @@ import shutil
 from pathlib import Path
 from unittest.mock import Mock
 from satellite_downloader.utils.connection import connect
+from satellite_downloader.extract_reanalysis import _format_dates, BR_AREA, DATA_DIR
 
 class TestDownloaderConnection(unittest.TestCase):
     uid = '123456'
@@ -38,7 +39,51 @@ class TestDownloaderConnection(unittest.TestCase):
                 key,
                 f'key: {self.uid}:{self.key}'
                 )
-        
+
+
+class TestExtractMethods(unittest.TestCase):
+    def test_brazil_coordinates_to_copernicus_api(self):
+        self.assertEquals(BR_AREA, {'N': 5.5, 'W': -74.0, 'S': -33.75, 'E': -32.25})
+    
+    def test_default_data_directory(self):
+        self.assertEquals(DATA_DIR, Path().home / 'copernicus_data')
+
+    def test_date_formatting(self):
+        self.assertEquals(
+            _format_dates('2023-01-01'),
+            ('2023', '01', '01')
+        )
+        self.assertEquals(
+            _format_dates('2023-01-01','2023-01-02'),
+            ('2023', ['01'], ['01', '02'])
+        )
+        self.assertEquals(
+            _format_dates('2023-01-01','2023-01-31'),
+            (
+                '2023', 
+                ['01'], 
+                [
+                    '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+                    '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+                    '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+                    '31'
+                ]
+            )
+        )
+        self.assertEquals(
+            _format_dates('2023-01-01','2023-06-15'),
+            (
+                '2023', 
+                ['01', '02', '03', '04', '05', '06'], 
+                [
+                    '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+                    '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+                    '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+                    '31'
+                ]
+            )
+        )
+    ...
 
 if __name__ == '__main__':
     unittest.main()
