91a92e1f2ea0ff477408dc711e1232339c6a8bd6
Luã Bida Vacaro <luabidaa@gmail.com>
2023-08-23 15:18:44 -0300

    feat(downloader): implement global fetch on satellite downloader (area)

--- satellite/downloader/__init__.py
+++ satellite/downloader/__init__.py
@@ -2,4 +2,7 @@
 """satellite_weather_downloader Weather Collection Python package"""
 # TODO: Docstrings
 from . import request  # noqa
-from .extract_reanalysis import download_br_netcdf  # noqa
+from .extract_reanalysis import (
+    download_br_netcdf,
+    download_netcdf
+)
