7815ef312303de73e88229ceeef9fb40eff49f25
Luã Bida Vacaro <luabidaa@gmail.com>
2024-10-22 19:37:58 -0300

    Fix cls instantiation on classmethod get()

--- satellite/weather/orm/models.py
+++ satellite/weather/orm/models.py
@@ -4,26 +4,47 @@ from abc import ABC, abstractmethod
 import pandas as pd
 import duckdb
 
-from satellite.weather.orm import functional, constants, types
+from satellite.weather.orm import functional, types
 
 
 def init_db():
-    ADM0.drop_table()
-    ADM0.create_table()
+    for adm in [ADM0, ADM1]:
+        adm.drop_table()
+        adm.create_table()
 
-    df = pd.DataFrame.from_dict({"code": ["BRA"], "full_name": ["Brazil"]})
+    df = pd.DataFrame.from_dict({"code": ["BRA"], "name": ["Brazil"]})
 
     with functional.session() as session:
         session.execute(f"INSERT INTO {ADM0.__tablename__} SELECT * FROM df")
         session.commit()
 
     bra = ADM0.get(code="BRA")
-    print(bra.full_name)
+    assert bra.name == "Brazil"
+
+    df = pd.DataFrame.from_dict(
+        {"code": [123], "name": ["SP"], "adm0": ["BRA"]}
+    )
+
+    with functional.session() as session:
+        session.execute(f"INSERT INTO {ADM1.__tablename__} SELECT * FROM df")
+        session.commit()
+
+    sp = ADM1.get(code=123)
+    assert sp.name == "SP"
 
 
 class Base(ABC):
     __tablename__: str
-    __fields__: list[str]  # NOTE: Must be indexed as the same
+    __fields__: list[str]  # NOTE: Must be indexed as the same as the attrs
+
+    code: str | int
+    name: str
+
+    def __str__(self) -> str:
+        return str(self.code)
+
+    def __repr__(self) -> str:
+        return self.name
 
     @abstractmethod
     def __init__(self) -> None: ...
@@ -34,23 +55,45 @@ class Base(ABC):
     @classmethod
     def get(cls, **params):
         with functional.session() as session:
-            data = session.sql(f"""
-                SELECT * FROM {cls.__tablename__} 
-                WHERE {" AND ".join([f"{p} = {v}" for p, v in params.items()])}'
-            """).fetchone()
+            res = session.sql(f"""
+                SELECT * FROM {cls.__tablename__} WHERE 
+                {" AND ".join([f"{p} = '{v}'" for p, v in params.items()])}
+            """)
+
+            if len(res) > 1:
+                raise ValueError(
+                    f"{cls} for query {params} found multiple entries"
+                )
+
+            data = res.fetchone()
+
         if not data:
             raise ValueError(f"{cls} for query {params} not found")
 
-        return cls(**data)
+        instance = cls.__new__(cls)
+
+        for field, value in zip(cls.__fields__, data):
+            setattr(instance, field, value)
+
+        return instance
 
     @classmethod
     def create_table(cls):
         fields = cls._get_class_fields(cls.__fields__)  # noqa
         columns = []
         for field, _type in fields.items():
+            # TODO: should add fk?
+            if _type == ADM0:
+                _type = str
+
+            if _type in [ADM1]:
+                _type = int
+
             query = f"{field} {duckdb.typing.DuckDBPyType(_type)}"
+
             if field == "code":
                 query += " PRIMARY KEY"
+
             columns.append(query)
 
         with functional.session() as session:
@@ -78,27 +121,44 @@ class Base(ABC):
 
 class ADM0(Base):
     __tablename__ = "adm0"
-    __fields__ = ["code", "full_name"]
+    __fields__ = ["code", "name"]
 
     code: str
-    full_name: str
+    name: str
 
-    def __init__(self, code: types.ADM0Options, full_name: str) -> None:
+    def __init__(
+        self,
+        code: types.ADM0Options,
+        name: str,
+    ) -> None:
         self.code = code
-        self.full_name = full_name
+        self.name = name
 
-#
-# class ADM1(Base):
-#     __tablename__ = "ADM1"
-#
-#     id = Column(Integer, Sequence("ADM1_id_sequence"), primary_key=True)
-#     code = Column(Integer, nullable=False)
-#     full_name = Column(String, nullable=False)
-#     adm0_id = Column(
-#         Integer,
-#         ForeignKey("ADM0.id", ondelete="RESTRICT"),
-#         nullable=False
-#     )
+    def geometry(self):
+        raise NotImplementedError()
+
+
+class ADM1(Base):
+    __tablename__ = "adm1"
+    __fields__ = ["code", "name", "adm0"]
+
+    code: int
+    name: str
+    adm0: ADM0
+
+    def __init__(
+        self,
+        code: int,
+        name: str,
+        adm0: types.ADM0Options,
+    ) -> None:
+        self.code = code
+        self.name = name
+        self.adm0 = ADM0.get(code=adm0)
+
+    def geometry(self):
+        raise NotImplementedError()
+
 #
 #
 # class ADM2(Base):
@@ -106,7 +166,7 @@ class ADM0(Base):
 #
 #     id = Column(Integer, Sequence("ADM2_id_sequence"), primary_key=True)
 #     code = Column(Integer, nullable=False)
-#     full_name = Column(String, nullable=False)
+#     name = Column(String, nullable=False)
 #     adm1_id = Column(
 #         Integer,
 #         ForeignKey("ADM1.id", ondelete="RESTRICT"),
--- satellite/weather/orm/types.py
+++ satellite/weather/orm/types.py
@@ -5,5 +5,5 @@ from pydantic.functional_validators import AfterValidator
 
 from satellite.weather.orm import validations as v
 
-ADM0Options = Annotated[Optional[str], AfterValidator(v.validate_adm0_options)]
+ADM0Options = Annotated[str, AfterValidator(v.validate_adm0_options)]
 DatasetFile = Annotated[str, AfterValidator(v.validate_xr_dataset_file_path)]
