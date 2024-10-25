102258db10933f18691e7e2497d176ab3a964fe0
Luã Bida Vacaro <luabidaa@gmail.com>
2024-10-25 16:44:35 -0300

    add verbose fields on reanalysis request

--- satellite/request.py
+++ satellite/request.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from typing import Optional, Dict, Literal
+from datetime import datetime, timedelta
 from pathlib import Path
 
 import xarray as xr
@@ -10,10 +11,41 @@ from satellite.models import ERA5LandRequest, DataSet
 def reanalysis_era5_land(
     output: Optional[str] = None,
     client: Optional[Client] = None,
-    **kwargs,
+    product_type: list[str] = ["reanalysis"],
+    variable: list[str] = [
+        "2m_temperature",
+        "total_precipitation",
+        "2m_dewpoint_temperature",
+        "surface_pressure",
+    ],
+    date: str = str((datetime.now() - timedelta(days=6)).date()),
+    time: list[str] = [
+        "00:00",
+        "03:00",
+        "06:00",
+        "09:00",
+        "12:00",
+        "15:00",
+        "18:00",
+        "21:00",
+    ],
+    locale: Optional[Literal["BRA", "ARG"]] = None,
+    area: Optional[Dict[Literal["N", "S", "W", "E"], float]] = None,
+    format: Literal["grib", "netcdf"] = "netcdf",
+    download_format: Literal["zip", "unarchived"] = "zip",
 ) -> xr.Dataset:
+    request = dict(
+        product_type=product_type,
+        variable=variable,
+        date=date,
+        time=time,
+        locale=locale,
+        area=area,
+        format=format,
+        download_format=download_format,
+    )
     if output and Path(output).is_file():
         return DataSet.from_netcdf(output)
     return DataSet.from_netcdf(
-        ERA5LandRequest(client=client, request=kwargs).download(output)
+        ERA5LandRequest(client=client, request=request).download(output)
     )
